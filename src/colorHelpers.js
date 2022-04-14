bfca8b8b3243ddb5001ba1bad69dcfe7ffaed2c4
NimaAskari <nimaa.dev@gmail.com>
2022-04-14 17:28:37 +0000

    Add color slider using rc-slider

--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -6,7 +6,7 @@ function generatePalette(starterPalette) {
     paletteName: starterPalette.paletteName,
     id: starterPalette.id,
     emoji: starterPalette.emoji,
-    colors: {},
+    colors: {}
   };
   for (let level of levels) {
     newPalette.colors[level] = [];
@@ -17,24 +17,33 @@ function generatePalette(starterPalette) {
       newPalette.colors[levels[i]].push({
         name: `${color.name} ${levels[i]}`,
         id: color.name.toLowerCase().replace(/ /g, "-"),
+        hex: scale[i],
         rgb: chroma(scale[i]).css(),
         rgba: chroma(scale[i])
           .css()
           .replace("rgb", "rgba")
-          .replace(")", ",1.0)"),
+          .replace(")", ",1.0)")
       });
     }
   }
   return newPalette;
 }
-
 function getRange(hexColor) {
   const end = "#fff";
-  return [chroma(hexColor).darken(1.4).hex(), hexColor, end];
+  return [
+    chroma(hexColor)
+      .darken(1.4)
+      .hex(),
+    hexColor,
+    end
+  ];
 }
 
 function getScale(hexColor, numberOfColors) {
-  return chroma.scale(getRange(hexColor)).mode("lab").colors(numberOfColors);
+  return chroma
+    .scale(getRange(hexColor))
+    .mode("lab")
+    .colors(numberOfColors);
 }
 
 export { generatePalette };
