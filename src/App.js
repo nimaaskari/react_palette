ff81955b9a632ddb5782ff5ffce0c196e1966487
NimaAskari <nimaa.dev@gmail.com>
2022-04-18 11:10:15 +0000

    Fix routing errors and installing version 5.3.0 of react router dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import react, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
+import { Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" />
-      <Route exact path="/pelette/:id" />
-    </Switch>
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            ></Palette>
+          )}
+        />
+      </Switch>
 
-    /* {<div>
+      /* {<div>
       <Palette palette={generatePalette(seedColors[4])} />
     </div>} */
-  );
+    );
+  }
 }
 
 export default App;
